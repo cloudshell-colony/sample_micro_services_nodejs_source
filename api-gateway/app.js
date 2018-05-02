a102ac3180d2756a4176c294a9008d63497677b0
menibesso <meni.b@qualisystems.com>
2018-05-02 10:49:17 +0300

    update

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -4,27 +4,12 @@ const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
-app.use(express.static(__dirname));
 
 const paymentServiceIp = process.argv[2];
 const orderServiceIp = process.argv[3];
 
-
-app.get('/',function(req,res){
-    res.sendFile('index.html');
-});
-
 app.route('/orders')
     .get(function (req, res) {
-        var order = {
-            uri: 'http://' + orderServiceIp + '/orders',
-            method: 'POST',
-            json: { item: "ORDER SUCCESS!", price: 100, customer: "NEW CUSTOMER" }
-        };
-        request(order, function(err1, res1, body1){
-            res.json(body1);
-        });
-
         request.get('http://' + orderServiceIp + '/orders').on('response', function(res1) {
             res1.on('data', function(data1) {
                 request.get('http://' + paymentServiceIp + '/transactions').on('response', function(res2) {
@@ -32,13 +17,13 @@ app.route('/orders')
                         var result = [];
                         var orders = JSON.parse(data1);
                         var transactions = JSON.parse(data2);
-                            
+
                         orders.forEach(function(order) {
                             transactions.forEach(function (transaction) {
                                 if(order.id === transaction.orderId) {
-                                    result.push({id: order.id, 
-                                        price: order.price, 
-                                        customer: order.customer, 
+                                    result.push({id: order.id,
+                                        price: order.price,
+                                        customer: order.customer,
                                         success: transaction.success})
                                 }
                             })
@@ -49,5 +34,15 @@ app.route('/orders')
             })
         })
     })
+    .post(function (req, res) {
+        var order = {
+            uri: 'http://' + orderServiceIp + '/orders',
+            method: 'POST',
+            json: req.body
+        };
+        request(order, function(err1, res1, body1){
+            res.json(body1);
+        });
+    });
 
 app.listen(3000);
